4b11167a318c15dbe62bf94d79b45f4dd11b412f
Debbie Elliott <rigbydebbie@icloud.com>
2024-01-18 16:36:20 +0200

    removed commented out code

--- Calculator/app/src/main/java/com/delliott/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/delliott/calculator/MainActivity.kt
@@ -13,11 +13,7 @@ private const val STATE_OPERAND_STORED = "operandStored"
 
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var result: EditText //non nullable variable initiated later
-//    private lateinit var newNumber: EditText
-//    private val displayOperation: TextView by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.operation) }
 
-    //variables to hold operands and types of calculations
     private var operand1: Double? = null
     private var pendingOperation = "="
 
@@ -26,29 +22,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-//        result = findViewById(R.id.result)
-//        newNumber = findViewById(R.id.newNumber)
-//
-//        // data input buttons
-//        val button0: Button = findViewById(R.id.button0)
-//        val button1: Button = findViewById(R.id.button1)
-//        val button2: Button = findViewById(R.id.button2)
-//        val button3: Button = findViewById(R.id.button3)
-//        val button4: Button = findViewById(R.id.button4)
-//        val button5: Button = findViewById(R.id.button5)
-//        val button6: Button = findViewById(R.id.button6)
-//        val button7: Button = findViewById(R.id.button7)
-//        val button8: Button = findViewById(R.id.button8)
-//        val button9: Button = findViewById(R.id.button9)
-//        val buttonDot: Button = findViewById(R.id.buttonDot)
-//
-//        //operation buttons
-//        val buttonEquals: Button = findViewById(R.id.buttonEqual)
-//        val buttonDivide: Button = findViewById(R.id.buttonDivide)
-//        val buttonMultiply: Button = findViewById(R.id.buttonMultiply)
-//        val buttonMinus: Button = findViewById(R.id.buttonMinus)
-//        val buttonPlus: Button = findViewById(R.id.buttonPlus)
 
         val listener = View.OnClickListener { v ->
             val b = v as Button
