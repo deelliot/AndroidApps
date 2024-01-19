c3347bb077aa60016fd6ce0b6fcd45ace145120b
Debbie Elliott <rigbydebbie@icloud.com>
2024-01-19 13:54:57 +0200

    init commit

---
+++ Top10Downloader/app/src/main/java/com/delliott/top10downloader/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.delliott.top10downloader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
