4d5557f97e96dc4c620d4b639f107e280794e594
Debbie Elliott <rigbydebbie@icloud.com>
2024-02-03 18:20:23 +0200

    added repository and API service

---
+++ WhatsCooking/app/src/main/java/com/delliott/whatscooking/data/ApiService.kt
@@ -0,0 +1,39 @@
+package com.delliott.whatscooking.data
+
+import com.squareup.moshi.Moshi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+object ApiServiceProvider {
+    private const val baseUrl = "https://dummyjson.com/"
+
+    val client: ApiService by lazy { //only creates variable when called
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY) //prints out requests and responses
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        Retrofit.Builder().baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(
+                MoshiConverterFactory.create(
+                    Moshi.Builder().build()
+                )
+            )
+            .build().create(ApiService::class.java)
+    }
+}
+
+interface ApiService {
+    @GET("recipes")
+    suspend fun fetchAllRecipes(): AllRecipesSearchResponse
+    @GET("recipes")
+    suspend fun fetchRecipesByMeal(
+        @Query(value = "meal-type") mealType: String
+    ): AllRecipesSearchResponse
+}
---
+++ WhatsCooking/app/src/main/java/com/delliott/whatscooking/data/RecipesRepository.kt
@@ -0,0 +1,27 @@
+package com.delliott.whatscooking.data
+
+class RecipeRepository {
+    suspend fun getAllRecipes(): NetworkResult<List<Recipe>> {
+        try {
+            val result = ApiServiceProvider.client.fetchAllRecipes()
+            return NetworkResult.ApiSuccess(result.recipes)
+        } catch (e: Exception) {
+            return NetworkResult.ApiException(e)
+        }
+    }
+
+    suspend fun getRecipebyMealType(mealType: String) : NetworkResult<List<Recipe>> {
+        try {
+            val result = ApiServiceProvider.client.fetchRecipesByMeal(mealType)
+            return NetworkResult.ApiSuccess(result.recipes)
+        } catch (e: Exception) {
+            return NetworkResult.ApiException(e)
+        }
+    }
+}
+
+sealed class NetworkResult<T : Any> {
+    class ApiSuccess<T : Any>(val data: T) : NetworkResult<T>()
+    class ApiError<T : Any>(val code: Int, val message: String?) : NetworkResult<T>()
+    class ApiException<T : Any>(val e: Throwable) : NetworkResult<T>()
+}
