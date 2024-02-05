796eb424da85cd0147b51ba1e06e7d5b160d4635
Debbie Elliott <rigbydebbie@icloud.com>
2024-02-05 13:22:07 +0200

    init of new screen

---
+++ WhatsCooking/app/src/main/java/com/delliott/whatscooking/ui/composables/FullRecipeScreen.kt
@@ -0,0 +1,15 @@
+package com.delliott.whatscooking.ui.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun FullRecipeScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FullRecipeScreenPreview() {
+    FullRecipeScreen()
+}
