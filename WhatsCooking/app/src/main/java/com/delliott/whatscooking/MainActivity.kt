614320abf67e1bd33878a6061c1901b5855903e3
Debbie Elliott <rigbydebbie@icloud.com>
2024-02-05 13:20:21 +0200

    added topappbar and navController

--- WhatsCooking/app/src/main/java/com/delliott/whatscooking/MainActivity.kt
+++ WhatsCooking/app/src/main/java/com/delliott/whatscooking/MainActivity.kt
@@ -3,14 +3,32 @@ package com.delliott.whatscooking
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.delliott.whatscooking.ui.HomeScreenViewModel
 import com.delliott.whatscooking.ui.composables.HomeScreen
 import com.delliott.whatscooking.ui.theme.WhatsCookingTheme
@@ -31,23 +49,83 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+enum class RecipeScreen(@StringRes val title: Int) {
+    HomeScreen(title = R.string.app_name),
+    FullRecipeScreen(title = R.string.FullRecipeScreen)
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun AppBar(
+    currentScreen: RecipeScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
 @Composable
 fun WhatsCookingApp(
-    viewModel: HomeScreenViewModel = viewModel()
-    // navController: NavHostController = rememberNavController()
+    viewModel: HomeScreenViewModel = viewModel(),
+    navController: NavHostController = rememberNavController(),
 ) {
-    val uiState by viewModel.uiState.collectAsState()
-    if (uiState.errorMessage != null) {
-        //todo
-    } else {
-        HomeScreen(
-            recipes30Mins = uiState.recipes30mins,
-            recipesTopRated = uiState.recipesTopRated,
-            onMealTypeSelected = { mealType: String ->
-                viewModel.fetchRecipesByMealType(mealType)
-            }
-        )
-    }
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = RecipeScreen.valueOf(
+        backStackEntry?.destination?.route ?: RecipeScreen.HomeScreen.name
+    )
+    Scaffold(
+        topBar = {
+            AppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
 
+        NavHost(
+            navController = navController,
+            startDestination = RecipeScreen.HomeScreen.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(RecipeScreen.HomeScreen.name) {
+                if (uiState.errorMessage != null) {
+                    // TODO:
+                }
+                HomeScreen(
+                    recipesTopRated = uiState.recipesTopRated,
+                    recipes30Mins = uiState.recipes30mins,
+                    onMealTypeSelected = { mealType: String ->
+                        viewModel.fetchRecipesByMealType(mealType)
+                    },
+                    onRecipeSelected = {
+                        viewModel.fetchRecipeDetails()
+                        navController.navigate(RecipeScreen.FullRecipeScreen.name)
+                    },
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.padding_medium))
+                )
+            }
+        }
+
+    }
 }
 
