086f2879a4b073f821bef17810bff9493ca48358
Debbie Elliott <rigbydebbie@icloud.com>
2024-04-08 12:55:48 +0300

    FAB to add new recipes

--- WhatsCooking/app/src/main/java/com/delliott/whatscooking/ui/home/composables/AddRecipeFAB.kt
+++ WhatsCooking/app/src/main/java/com/delliott/whatscooking/ui/home/composables/AddRecipeFAB.kt
@@ -1,7 +1,6 @@
 package com.delliott.whatscooking.ui.home.composables
 
 import android.content.Context
-import android.widget.Toast
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -16,16 +15,12 @@ import androidx.compose.ui.unit.dp
 fun AddRecipeFAB(modifier: Modifier, context: Context, onClick: () -> Unit) {
     FloatingActionButton(
         modifier = modifier,
-        onClick = {
-            Toast.makeText(context, "Floating action button", Toast.LENGTH_LONG).show()
-            onClick()
-        },
+        onClick = { onClick() },
         containerColor = MaterialTheme.colorScheme.primaryContainer,
         contentColor = MaterialTheme.colorScheme.secondary,
         elevation = FloatingActionButtonDefaults.elevation(
             defaultElevation = 8.dp
         )
-//        shape = CircleShape
     ) {
         Icon(Icons.Filled.Add, "Add Recipe Floating Action Button.")
     }
