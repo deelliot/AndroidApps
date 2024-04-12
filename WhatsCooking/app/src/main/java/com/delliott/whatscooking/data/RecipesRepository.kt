e93d386c24689f5a8abd45c0cfc170b25070ea45
Debbie Elliott <rigbydebbie@icloud.com>
2024-04-12 10:46:38 +0300

    added dao to repository

--- WhatsCooking/app/src/main/java/com/delliott/whatscooking/data/RecipesRepository.kt
+++ WhatsCooking/app/src/main/java/com/delliott/whatscooking/data/RecipesRepository.kt
@@ -1,8 +1,13 @@
 package com.delliott.whatscooking.data
 
 import android.util.Log
+import com.delliott.whatscooking.dao.RecipeDao
+import com.delliott.whatscooking.dao.RecipeEntity
+import com.delliott.whatscooking.domain.NewRecipeModel
+import java.util.UUID
 
-class RecipeRepository {
+class RecipeRepository(private val recipeDao: RecipeDao) {
+
     suspend fun getAllRecipes(): NetworkResult<List<Recipe>> {
         return try {
             val result = ApiServiceProvider.client.fetchAllRecipes()
@@ -12,7 +17,7 @@ class RecipeRepository {
         }
     }
 
-    suspend fun getRecipeByMealType(mealType: String) : NetworkResult<List<Recipe>> {
+    suspend fun getRecipeByMealType(mealType: String): NetworkResult<List<Recipe>> {
         return try {
             val result = ApiServiceProvider.client.fetchRecipesByMeal(mealType)
             NetworkResult.ApiSuccess(result.recipes)
@@ -21,7 +26,7 @@ class RecipeRepository {
         }
     }
 
-    suspend fun getRecipeDetails(recipeId: Int) : NetworkResult<RecipeDetailResponse> {
+    suspend fun getRecipeDetails(recipeId: Int): NetworkResult<RecipeDetailResponse> {
         Log.d("TEST", "LOADING")
         return try {
             val result = ApiServiceProvider.client.fetchRecipeDetails(recipeId)
@@ -31,13 +36,37 @@ class RecipeRepository {
         }
     }
 
-    suspend fun getSearchRecipes(searchTerm: String) : NetworkResult<List<Recipe>> {
+    suspend fun getSearchRecipes(searchTerm: String): NetworkResult<List<Recipe>> {
         return try {
             val result = ApiServiceProvider.client.fetchSearchRecipes(searchTerm)
             NetworkResult.ApiSuccess(result.recipes)
         } catch (e: Exception) {
             NetworkResult.ApiException(e)
         }
+    }
+
+    suspend fun saveRecipe(newRecipe: NewRecipeModel) {
+        val recipe = convertNewRecipe(newRecipe)
+        recipeDao.upsertRecipe(recipe)
+    }
+
+    private fun convertNewRecipe(newRecipe: NewRecipeModel): RecipeEntity {
+
+        val ingredientsString: String = newRecipe.ingredients.value.joinToString(prefix = "[", postfix = "]", separator = ", ")
+        val instructionsString: String = newRecipe.instructions.value.joinToString(prefix = "[", postfix = "]", separator = ", ")
+
+        return RecipeEntity(
+            id = UUID.randomUUID().toString(),
+            name = newRecipe.name.value,
+            ingredients = ingredientsString,
+            instructions = instructionsString,
+            prepTime = newRecipe.prepTimeMinutes.value,
+            cookTime = newRecipe.cookTimeMinutes.value,
+            servings = newRecipe.servings.value,
+            cuisine = newRecipe.cuisine.value,
+            image = newRecipe.image,
+            totalTime = newRecipe.totalTime,
+        )
     }
 }
 
